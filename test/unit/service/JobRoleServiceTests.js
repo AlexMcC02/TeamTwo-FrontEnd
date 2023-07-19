16a4cd9743861ba39b532f5d56182e584c44299d
AlexMcC02 <alex.mccullough@kainos.com>
2023-07-19 12:58:51 +0100

    test: added unit test for viewing job roles

---
+++ test/unit/service/JobRoleServiceTests.js
@@ -0,0 +1,29 @@
+var axios = require('axios');
+var MockAdapter = require('axios-mock-adapter')
+var chai = require('chai')
+const expect = chai.expect
+const JobRoleService = require('../../../service/JobRoleService')
+
+const jobrole = {
+    id: "1",
+    name: "Software Engineer",
+    specification: "Does coding.",
+    capabilityID: "1",
+    bandID: "1"
+}
+
+describe('JobRoleService', function () {
+    describe('getJobRoles', function () {
+        it('should return jobroles from response', async () => {
+            var mock = new MockAdapter(axios);
+    
+            const data = [jobrole];
+    
+            mock.onGet(JobRoleService.URL).reply(200, data);
+    
+            var results = await JobRoleService.getJobRoles();
+    
+            expect(results[0]).to.deep.equal(jobrole)
+          })
+    })
+})
