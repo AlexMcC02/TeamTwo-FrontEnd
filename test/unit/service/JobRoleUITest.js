71ada93ebe56c8fa7e04d92c54d2d314cee40564
philipwilson5 <philip.wilson@kainos.com>
2023-07-20 10:39:11 +0100

    test: changes to how success/failure display, # needs environment variable #

--- test/unit/service/JobRoleUITest.js
+++ test/unit/service/JobRoleUITest.js
@@ -1,1 +1,30 @@
 
+
+async function JobRolesUITest() {
+
+  const driver = await new Builder().forBrowser('chrome').build();
+
+  try {
+
+    await driver.get('http://localhost:3000/job_roles');
+
+    const textElement = await driver.findElement(By.xpath("//h1[normalize-space()='View Job Roles']"));
+
+    const actualText = await textElement.getText();
+
+    const expectedText = 'View Job Roles';
+
+    if (actualText === expectedText) {
+      console.log('\u001B[32m✔ Job Role UI Test Successful, "View Job Roles" found\u001B[0m');
+    } else {
+      console.log('\u001B[31mTest Failed. Expected:', expectedText, 'Actual:', actualText, '\u001B[0m');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  } finally {
+
+    await driver.quit();
+  }
+}
+
+JobRolesUITest();
