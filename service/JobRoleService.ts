a86991823a78a5b3ef7103c2963e774118291e21
AlexMcC02 <alex.mccullough@kainos.com>
2023-07-21 12:46:34 +0100

    refactor: service class now uses an env var

--- service/JobRoleService.ts
+++ service/JobRoleService.ts
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports.getJobRoles = async function() {
     try {
-        const response = await axios.get('http://localhost:8080/api/job_roles')
+        const response = await axios.get(process.env.UI_URL + '/api/job_roles')
         return response.data
     } catch (e) {
         throw new Error('Could not get job roles.')
