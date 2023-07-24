5abc7af2b003e78bac3105092685c8ef83e26365
AlexMcC02 <alex.mccullough@kainos.com>
2023-07-24 09:42:14 +0100

    feat: added missing unit test

--- service/JobRoleService.ts
+++ service/JobRoleService.ts
@@ -5,7 +5,7 @@ module.exports.getJobRoles = async function() {
         const response = await axios.get(process.env.UI_URL + '/api/job_roles')
         return response.data
     } catch (e) {
-        throw new Error('Could not get job roles.')
+        return new Error('Could not get job roles.')
     }
 }
 
