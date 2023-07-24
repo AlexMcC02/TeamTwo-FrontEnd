a71383c07cfedbef7bce26d52598e8212d557aec
Kamil <kamil.biegaj@kainos.com>
2023-07-24 09:33:13 +0100

    Initial git repo for UI

--- app.ts
+++ app.ts
@@ -1,59 +1,31 @@
-import { Request, Response } from "express";
-import session = require("express-session");
-import { Login } from "./model/auth";
+import { Application } from "express";
 
-const express = require("express");
-const path = require("path");
-const nunjucks = require("nunjucks");
-const app = express();
+const express = require('express')
+const path = require('path')
+const nunjucks = require('nunjucks')
 
-const appViews = path.join(__dirname, "/views/");
+const app: Application = express();
+
+const appViews = path.join(__dirname, '/views/')
 
 const nunjucksConfig = {
-  autoescape: true,
-  noCache: true,
-  express: app,
-};
-
-nunjucks.configure(appViews, nunjucksConfig);
-
-app.set("view engine", "html");
-
-app.use("/public", express.static(path.join(__dirname, "public")));
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({secret: 'NOT HARDCODED SECRET', cookie: {maxAge: 60000}}));
-
-declare module "express-session" {
-  interface SessionData {
-    token: string
-  }
+    autoescape: true,
+    noCache: true,
+    express: app
 }
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+nunjucks.configure(appViews, nunjucksConfig)
 
-app.use(express.json());
+app.set('view engine', 'html')
 
-app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
-  console.log("Server listening at port 3000");
-});
+    console.log('Server listening on port 3000')
+})
 
-app.get("/index", async (req: Request, res: Response) => {
-  res.render("index", {
-    title: "Index",
-  });
-});
+app.get('/', (req, res) => {
+    res.render('list-jobroles')
+})
 
-require("./controller/authController")(app);
-const authMiddleware = require('./middleware/auth')
-app.use(authMiddleware)
-
-require('./controller/salesEmployeeController')(app)
-require("./controller/clientController")(app);
-require("./controller/projectController")(app);
-require("./controller/deliveryEmployeeController")(app);
+require("./controller/JobRoleController")(app);
