9c5778e65d067f6dcacd2b0cbb3ce96145682276
philipwilson5 <philip.wilson@kainos.com>
2023-07-24 14:09:16 +0100

    fix: duplication in app.ts

--- app.ts
+++ app.ts
@@ -1,41 +1,34 @@
+import { Application } from "express";
 const express = require('express')
 const path = require('path')
 const nunjucks = require('nunjucks')
+const app: Application = express();
 
-const app = express();
-
-const appViews = path.join(__dirname, '/views/')
 const appViews = path.join(__dirname, '/views/')
 
 const nunjucksConfig = {
     autoescape: true,
     noCache: true,
     express: app
-    autoescape: true,
-    noCache: true,
-    express: app
 }
 
 nunjucks.configure(appViews, nunjucksConfig)
-nunjucks.configure(appViews, nunjucksConfig)
+
 
 app.set('view engine', 'html')
-app.set('view engine', 'html')
+
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use('/public', express.static(path.join(__dirname, 'public')))
+
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000')
 })
-    console.log('Server listening on port 3000')
-})
+
 
 app.get('/', (req, res) => {
     res.render('list-jobroles')
 })
-app.get('/', (req, res) => {
-    res.render('list-jobroles')
-})
+
 
 require("./controller/JobRoleController")(app)
