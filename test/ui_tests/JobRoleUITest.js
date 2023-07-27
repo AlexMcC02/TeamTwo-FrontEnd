bfbc60575a5ffe9653e8055bfec34125f2bf8647
philipwilson5 <philip.wilson@kainos.com>
2023-07-27 10:22:23 +0100

    fix: changes to test

--- test/ui_tests/JobRoleUITest.js
+++ test/ui_tests/JobRoleUITest.js
@@ -3,16 +3,16 @@ const chai = require('chai');
 
 
 
-describe('JobRolesUITest', async () => {
+describe('JobSpecificationUITest', async () => 
 
-  it('should check if elements on the JobRoles page are present', async () => {
+  it('should check if elements on the viewJobSpecification page are present', async () => {
 
     var driver = new webdriver.Builder().
       withCapabilities(webdriver.Capabilities.chrome()).
       build();
 
       await driver.get(process.env.UI_TEST_URL);
-      
+    //job role navigation & checks
       await driver.findElement(webdriver.By.id('header')).getText().then(function(value) {
         chai.assert.equal(value, 'View Job Roles')
       });
@@ -25,7 +25,31 @@ describe('JobRolesUITest', async () => {
       await driver.findElement(webdriver.By.id('specification')).getText().then(function(value) {
         chai.assert.equal(value, 'Specification')
       });
+
+
+      //specification Page Navigation
+      await driver.findElement(webdriver.By.linkText('Specification')).click();
+    
+
+
+    // specification page verification
+      await driver.findElement(webdriver.By.id('specificationHeader')).getText().then(function(value) {
+        chai.assert.equal(value, 'Specification')
+      });
+
+      await driver.findElement(webdriver.By.id('name')).getText().then(function(value) {
+        chai.assert.equal(value, 'Name')
+      });
+      
+      await driver.findElement(webdriver.By.id('specification')).getText().then(function(value) {
+        chai.assert.equal(value, 'Specification')
+      });
+
+      await driver.findElement(webdriver.By.id('backToSalesEmployeeButton')).getText().then(function(value) {
+        chai.assert.equal(value, 'Back to sales employees')
+      });
+
+      await driver.findElement(webdriver.By.id('backToSalesEmployeeButton')).click();
       
   await driver.quit();
-});
-});
+}));
