cba51233844fc4cd261a29b912d4fb1c33a0cc79
Conn Meehan <conn.meehan@kainos.com>
2023-07-31 13:51:36 +0100

    Tests passing

--- test/unit/controller/JobRoleControllerTests.ts
+++ test/unit/controller/JobRoleControllerTests.ts
@@ -30,4 +30,46 @@ describe('JobRoleController', function() {
             stub.restore()
         })
     })
+
+    describe('DELETE /job_roles/:id', async function () {
+        it('should delete the job role and render the updated list-jobroles view', async () => {
+            // Stub the jobRoleService.deleteJobRole function to return a resolved promise
+            const deletedJobRoleId = "1"; // The ID of the job role to be deleted
+            const mockDeletedJobRole = {
+                id: deletedJobRoleId.toString(),
+                name: 'Deleted Job Role',
+                specification: 'No longer exists'
+            };
+            const fakeDeleteJobRole = sinon.fake.resolves(mockDeletedJobRole);
+
+            // Replace the original deleteJobRole function with the fake one.
+            sinon.replace(jobRoleService, 'deleteJobRole', fakeDeleteJobRole);
+
+            // Mock the job role data for the response from service
+            const mockJobRoles = [mockDeletedJobRole];
+            sinon.replace(jobRoleService, 'getJobRoles', sinon.fake.returns(mockJobRoles));
+
+            // Send a DELETE request to the endpoint with the job role ID to delete
+            const response = await request
+                .delete(`/job_roles/${deletedJobRoleId}`)
+                .set('Accept', 'application/json')
+                .expect(200);
+
+            // Logging to check arguments used for the deleteJobRole function
+            console.log(fakeDeleteJobRole.getCall(0).args[0]); // Accessing the first call's first argument
+
+            // Logging the response from the DELETE request
+            console.log(response.text);
+
+            // Assertions
+            expect(response.text).to.contain('Deleted Job Role');
+            expect(response.text).to.contain('No longer exists');
+
+            // Verify that the jobRoleService.deleteJobRole was called with the correct job role ID
+            expect(fakeDeleteJobRole.getCall(0).args[0]).to.equal(deletedJobRoleId);
+
+            // Clean up: restore the original functions
+            sinon.restore();
+        })
+    })
 })
--- test/unit/service/JobRoleServiceTests.ts
+++ test/unit/service/JobRoleServiceTests.ts
@@ -22,17 +22,54 @@ describe('JobRoleService', function () {
             var results = await JobRoleService.getJobRoles();
     
             expect(results[0]).to.deep.equal(jobrole)
-          })
-    })
-    describe('getJobRoles', function () {
+        })
+
         it('should return exception when 500 error returned from axios', async () => {
             var mock = new MockAdapter(axios);
     
-            mock.onGet(JobRoleService.URL).reply(500);
+            mock.onGet(JobRoleService.URL).networkError();
     
             var error = await JobRoleService.getJobRoles();
     
             expect(error.message).to.equal('Could not get job roles.')
-          })
-    }) 
+        })
+    })
+    
+    describe('deleteJobRole', function () {
+        it('should delete a job role and return the deleted job role', async () => {
+            const deletedJobRoleId = "1";
+            const mockDeletedJobRole = {
+                id: deletedJobRoleId,
+                name: 'Deleted Job Role',
+                specification: 'No longer exists'
+            };
+
+            var mock = new MockAdapter(axios);
+
+            mock.onDelete().reply(200, mockDeletedJobRole);
+
+            try {
+                const result = await JobRoleService.deleteJobRole(deletedJobRoleId);
+                console.log('Result:', result); 
+                expect(result).to.deep.equal(mockDeletedJobRole);
+            } catch (error) {
+                throw new Error(`Failed to delete job role: ${error.message}`);
+            }
+        });
+
+        it('should return exception when deleting a job role returns a 500 error', async () => {
+            const deletedJobRoleId = "1";
+
+            var mock = new MockAdapter(axios);
+
+            mock.onDelete().reply(500);
+
+            try {
+                await JobRoleService.deleteJobRole(deletedJobRoleId);
+            } catch (error) {
+                console.log('Error:', error.message);
+                expect(error.message).to.equal('Could not delete job role.');
+            }
+        });
+    }); 
 })
